0ce5c30af4e058ac80bec8b3e7c6b8c5868e4201
OwenC525 <owencc8@gmail.com>
2024-09-30 08:48:48 -0400

    fixed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-    $('swatch').on('mouseenter', function() {
+    $('.swatch').on('mouseenter', function() {
         $('#product-photo').attr('src', 'img/chair-' + $(this).attr('id') + '@2x.jpg');
-        $('swatch').removeClass('selected');
+        $('.swatch').removeClass('selected');
         $(this).addClass('selected');
     });
 });
